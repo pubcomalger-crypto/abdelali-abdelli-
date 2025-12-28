e0b8295ec7a905cf7e8e9dec8284154e52d35915
pubcomalger-crypto <pubcom.alger@gmail.com>
2025-12-28 17:00:53 +0100

    ok
    ok

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const profileConfig = {
     // Informations de base
 
 
-    name: "COCA",
-    bio: "SALAM",
-    profileImage: "https://i.ibb.co/NgKMhgMT/cocacola.jpg",
+    name: "abdelali ",
+    bio: "importateur",
+    profileImage: "https://www.facebook.com/photo/?fbid=803516413405334&set=a.121263411630641",
     
 
 
@@ -15,14 +15,14 @@ const profileConfig = {
 
     // Réseaux sociaux
     socialLinks: {
-        facebook: "https://www.facebook.com/share/16dysjBQvt/?mibextid=wwXIfr",
+        facebook: "https://www.https://www.facebook.com/abdelali.abdelli.3/",
         instagram: "https://www.instagram.com/riyadmahrez26.7?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
         tiktok: "https://www.tiktok.com/@chebkhaled?is_from_webapp=1&sender_device=pc",
-        whatsapp: "+213540253369",
-        telegram: "+213 562 33 14 08",
+        whatsapp: "+213540732616",
+        telegram: "+213 540 73 26 16",
         linkedin: "daisyparkhouse",
         snapchat: "https://www.snapchat.com/@sheikh_tidiane?sender_web_id=792b2a4d-05d3-4e8f-a5e0-1aeb89eba722&device_type=desktop&is_copy_url=true",
-        email: "pubcom.alger@gmail.com",
+        email: "abdelaliabdelli45@gmail.com",
         website: "https://www.youtube.com/",
         maps: "https://share.google/T1z7NAFbftnLYNKZy",
         phone: "+213658546209"
@@ -38,14 +38,14 @@ const profileConfig = {
     enabledIcons: {
         facebook: true,      // Afficher Facebook
         instagram: true,     // Afficher Instagram
-        tiktok: true,        // Afficher TikTok
+        tiktok: false,        // Afficher TikTok
         whatsapp: true,      // Afficher WhatsApp
         telegram: true,      // Afficher Telegram
         linkedin: false,     // Afficher LinkedIn (mettre true pour l'activer)
         website: false,       // Afficher Site Web
-        maps: true,          // Afficher Maps
+        maps: false,          // Afficher Maps
         phone: true,         // Afficher Téléphone
-        snapchat: true,      // Afficher Snapchat
+        snapchat: false,      // Afficher Snapchat
         email: true,         // Afficher Email
     },
     
@@ -83,3 +83,4 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.profileConfig = profileConfig;
 }
+
